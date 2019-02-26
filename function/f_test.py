fa58fea9e4812430e8ff7a60de3371dee7ae3a91
DaringB <lucymariamisora0230@gmail.com>
2019-02-26 21:31:06 +0900

    lambda

---
+++ function/f_test.py
@@ -0,0 +1,16 @@
+"""
+　　  ∧ _ ∧
+　　 ( ˘ω˘ )　　起床！
+　 　|　⊃ ⊃
+　　 └-⊃～⊃
+　　　
+　　　｜｜　　　　　　　　
+　 ＿ _　　／(＿＿_
+／　 (＿＿＿_／　／
+
+@version: Python3.6.5
+@encoding: utf-8
+@author: Misora
+@createTime: 2018/12/20 21:12
+"""
+
